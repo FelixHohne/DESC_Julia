de8ec9e5b38c31cd9b93dfb38dd487c9d9c70887
Felix Hohne <felix.m.hohne@gmail.com>
2023-04-09 09:42:02 -0400

    Add solve_continuation

--- DESC.jl/src/descContinuation.jl
+++ DESC.jl/src/descContinuation.jl
@@ -1,3 +1,43 @@
+function jl_solve_continuation(
+    eqfam;
+    objective = "force", 
+    optimizer = "lsq-exact", 
+    pert_order = 2, 
+    ftol = nothing, 
+    xtol = nothing, 
+    nfev = 100, 
+    verbose = 1, 
+    checkpoint_path = nothing
+)
+    # TODO: Natively support kwargs
+    py"""
+    import numpy as np
+    import desc
+    import desc.continuation 
+
+    from desc import set_device
+    set_device('gpu')
+    
+    def create_solve_continuation():
+        return desc.continuation.solve_continuation(
+            eq = $eq, 
+            objective = $objective, 
+            optimizer = $optimizer, 
+            pert_order = $pert_order, 
+            ftol = $ftol, 
+            xtol = $xtol, 
+            nfev = $nfev, 
+            verbose = $verbose, 
+            checkpoint_path = $checkpoint_path
+        )
+    """
+    output = py"create_solve_continuation"()
+    return output 
+
+end 
+
+
+
 function jl_solve_continuation_automatic(
     eq;
     objective = "force", 
