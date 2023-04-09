5364741ac32f9fe9f7442a144cfde514497921c5
Felix Hohne <fmh42@cornell.edu>
2023-04-09 08:33:41 -0400

    Continue adding objectives

--- DESC.jl/src/descObjectives.jl
+++ DESC.jl/src/descObjectives.jl
@@ -14,6 +14,32 @@ function jl_objective_aspect_ratio(equilibrium::PyObject, target=2, weight=1, no
     output = py"create_objective_aspect_ratio"()
 end 
 
+function jl_objective_quasisymmetry_two_term(;
+    eq, 
+    target=0,  
+    bounds = nothing, 
+    weight=1, 
+    normalize=true, 
+    normalize_target=true, 
+    grid = nothing, 
+    helicity = (1, 0), 
+    name="QS two-term"
+)
+
+    py"""
+    import numpy as np
+    import desc
+    def create_objective_quasisymmetry_two_term():
+        # Not supporting grid
+        return desc.objectives.QuasisymmetryTwoTerm(
+            eq=$equilibrium, target=$target, weight=$weight, 
+            normalize=$normalize, normalize_target=$normalize_target, 
+            name=$name)
+    """
+    output = py"create_objective_quasisymmetry_two_term"()
+end 
+
+
 function jl_objective_fix_boundary_r(;
     eq = nothing, 
     target = nothing,  
@@ -39,6 +65,34 @@ function jl_objective_fix_boundary_r(;
     """
     output = py"create_objective_fix_boundary_r"()
 end 
+
+
+function jl_objective_fix_boundary_z(;
+    eq = nothing, 
+    target = nothing,  
+    bounds = nothing, 
+    weight = 1, 
+    normalize = true, 
+    normalize_target = true, 
+    fixed_boundary = false, 
+    modes = true, 
+    surface_label = nothing, 
+    name = "lcfs Z"
+)
+
+    py"""
+    import numpy as np
+    import desc
+    from desc.objectives.linear_objectives import *
+    def create_objective_fix_boundary_z():
+        return FixBoundaryR(
+            eq=$eq, target=$target, weight=$weight, 
+            normalize=$normalize, normalize_target=$normalize_target, 
+            name=$name)
+    """
+    output = py"create_objective_fix_boundary_z"()
+end 
+
 
 
 
@@ -68,7 +122,15 @@ function jl_objective_bootstrapRedlConsistency(
 end 
 
 
-function jl_objective_current_density(equilibrium, target = 0, bounds = nothing, weight = 1, normalize = true, normalize_target = true, grid = nothing, name = "current density")
+function jl_objective_current_density(
+    equilibrium = nothing, 
+    target = 0, 
+    bounds = nothing, 
+    weight = 1, 
+    normalize = true, 
+    normalize_target = true, 
+    grid = nothing, 
+    name = "current density")
     py"""
     import numpy as np
     import desc
@@ -80,12 +142,12 @@ function jl_objective_current_density(equilibrium, target = 0, bounds = nothing,
             normalize=$normalize, normalize_target=$normalize_target, 
             name=$name)
     """
-    output = py"create_objective_aspect_ratio"()
+    output = py"create_objective_current_density"()
 end 
 
 
 function jl_objective_elongation(
-    equilibrium, 
+    equilibrium = nothing, 
     target = 1, 
     bounds = nothing, 
     weight = 1, 
@@ -109,6 +171,191 @@ function jl_objective_elongation(
     output = py"create_objective_elongation"()
 
 end 
+
+
+function jl_objective_energy(;
+    eq = nothing, 
+    target = 0, 
+    bounds = nothing, 
+    weight = 1, 
+    normalize = true, 
+    normalize_target = true, 
+    grid = nothing, 
+    gamma = 0, 
+    name = "energy"
+)
+    py"""
+    import numpy as np
+    import desc
+    # TODO: Grid
+    def create_objective_energy():
+        # Not supporting grid
+        return desc.objectives.Energy(
+            eq=$eq, target=$target, bounds = $bounds, weight=$weight, 
+            normalize=$normalize, normalize_target=$normalize_target, 
+            grid=$grid, gamma=$gamma, 
+            name=$name)
+    """
+    output = py"create_objective_energy"()
+
+end 
+
+
+function jl_objective_force_balance(;
+    eq = nothing,  
+    target = 0, 
+    bounds = nothing, 
+    weight = 1, 
+    normalize = true, 
+    normalize_target = true, 
+    grid = nothing, 
+    name = "force"
+)
+    py"""
+    import numpy as np
+    import desc
+    # TODO: Grid
+    def create_objective_force_balance():
+        # Not supporting grid
+        return desc.objectives.ForceBalance(
+            eq=$eq, target=$target, bounds = $bounds, weight=$weight, 
+            normalize=$normalize, normalize_target=$normalize_target, 
+            grid=$grid, 
+            name=$name)
+    """
+    output = py"create_objective_force_balance"()
+end 
+
+
+function jl_objective_generic_objective(;
+    f; 
+    eq = nothing,  
+    target = 0, 
+    bounds = nothing, 
+    weight = 1, 
+    normalize = true, 
+    normalize_target = true, 
+    grid = nothing, 
+    name = "generic"
+)
+    py"""
+    import numpy as np
+    import desc
+    # TODO: Grid
+    def create_generic_objective():
+        # Not supporting grid
+        return desc.objectives.GenericObjective(
+            f=$f, eq=$eq, target=$target, bounds = $bounds, weight=$weight, 
+            normalize=$normalize, normalize_target=$normalize_target, 
+            grid=$grid, name=$name)
+    """
+    output = py"create_generic_objective"()
+end 
+
+function jl_objective_helical_force_balance(;
+    eq = nothing,  
+    target = 0, 
+    bounds = nothing, 
+    weight = 1, 
+    normalize = true, 
+    normalize_target = true, 
+    grid = nothing, 
+    name = "helical force"
+)
+    py"""
+    import numpy as np
+    import desc
+    # TODO: Grid
+    def create_helical_force_objective():
+        # Not supporting grid
+        return desc.objectives.HelicalForceBalance(
+            eq=$eq, target=$target, bounds = $bounds, weight=$weight, 
+            normalize=$normalize, normalize_target=$normalize_target, 
+            grid=$grid, name=$name)
+    """
+    output = py"create_helical_force_objective"()
+end 
+
+
+function jl_objective_magnetic_well(;
+    eq = nothing,  
+    target = nothing, 
+    bounds = (0, Inf), 
+    weight = 1, 
+    normalize = true, 
+    normalize_target = true, 
+    grid = nothing, 
+    name = "Magnetic Well"
+)
+    py"""
+    import numpy as np
+    import desc
+    # TODO: Grid
+    def create_magnetic_well_objective():
+        # Not supporting grid
+        return desc.objectives.MagneticWelle(
+            eq=$eq, target=$target, bounds = $bounds, weight=$weight, 
+            normalize=$normalize, normalize_target=$normalize_target, 
+            grid=$grid, name=$name)
+    """
+    output = py"create_magnetic_well_objective"()
+end 
+
+function jl_objective_mean_curvature(;
+    eq = nothing,  
+    target = nothing, 
+    bounds = (-Inf, 0), 
+    weight = 1, 
+    normalize = true, 
+    normalize_target = true, 
+    grid = nothing, 
+    name = "mean-curvature"
+)
+    py"""
+    import numpy as np
+    import desc
+    # TODO: Grid
+    def create_mean_curvature_objective():
+        # Not supporting grid
+        return desc.objectives.MeanCurvature(
+            eq=$eq, target=$target, bounds = $bounds, weight=$weight, 
+            normalize=$normalize, normalize_target=$normalize_target, 
+            grid=$grid, name=$name)
+    """
+    output = py"create_mean_curvature_objective"()
+end 
+
+
+function jl_objective_mercier_stability(;
+    eq = nothing,  
+    target = nothing, 
+    bounds = (0, Inf), 
+    weight = 1, 
+    normalize = true, 
+    normalize_target = true, 
+    grid = nothing, 
+    name = "Mercier Stability"
+)
+    py"""
+    import numpy as np
+    import desc
+    # TODO: Grid
+    def create_mercier_stability_objective():
+        # Not supporting grid
+        return desc.objectives.MercierStability(
+            eq=$eq, target=$target, bounds = $bounds, weight=$weight, 
+            normalize=$normalize, normalize_target=$normalize_target, 
+            grid=$grid, name=$name)
+    """
+    output = py"create_mercier_stability_objective"()
+end 
+
+
+
+
+
+
+
 
 
 
