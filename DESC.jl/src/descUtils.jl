27f74690540c356d1d4d425ad7a83bb10e51e154
Felix Hohne <fmh42@cornell.edu>
2023-04-17 08:07:16 -0400

    Better handle if no GPU is present

--- DESC.jl/src/descUtils.jl
+++ DESC.jl/src/descUtils.jl
@@ -2,9 +2,12 @@ using PyCall
 function desc_jl_use_gpu_if_available()
     py"""
     def use_gpu():
-        from desc import set_device
-        set_device('gpu')
+        import jax 
+        if jax.default_backend() == "gpu":
+            from desc import set_device
+            set_device('gpu')
         from desc.equilibrium import Equilibrium
     """
     py"use_gpu"()
 end
+
