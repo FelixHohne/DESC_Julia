e0fde1bf1083d049bf77149b262b55b7ae59efad
Felix Hohne <felix.m.hohne@gmail.com>
2023-04-17 08:25:13 -0400

    Clean descUtils.jl

--- DESC.jl/src/descUtils.jl
+++ DESC.jl/src/descUtils.jl
@@ -3,7 +3,6 @@ function desc_jl_use_gpu_if_available()
     py"""
     def use_gpu():
         import jax 
-        print(jax.default_backend())
         if jax.default_backend() == "gpu":
             from desc import set_device
             set_device('gpu')
