409881f1fa1ac5ac8e2409c0f1373d7134891905
Felix Hohne <fmh42@cornell.edu>
2023-04-17 06:03:43 -0400

    Push cleaner run tests

--- DESC.jl/test/runtests.jl
+++ DESC.jl/test/runtests.jl
@@ -3,9 +3,5 @@ using DESC
 using Test
 using PyCall
 
-
-# include("../install/install.jl")
 include("qs_tests.jl")
-include("qa_tests.jl")
-# include("utils_test.jl")
-# include("surface_test.jl")
+
